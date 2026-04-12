bb64e961edd423edc5d16330205c3ead36914240
newM1k3 <mikewalling@hotmail.ca>
2026-04-11 21:06:13 -0400

    chore: remove dead files

--- src/lib/pocketbase.ts
+++
@@ -1,9 +0,0 @@
-import PocketBase from 'pocketbase';
-
-const pbUrl = import.meta.env.VITE_POCKETBASE_URL;
-
-if (!pbUrl) {
-  throw new Error('Missing VITE_POCKETBASE_URL environment variable');
-}
-
-export const pb = new PocketBase(pbUrl);
